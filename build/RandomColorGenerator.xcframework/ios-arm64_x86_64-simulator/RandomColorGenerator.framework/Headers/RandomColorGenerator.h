793bdeeca245d42e9fa3eb5c2f244126391a975b
Apple <apple@Apples-Mac-mini.local>
2025-03-28 20:27:36 +0530

    initial commit

---
+++ build/RandomColorGenerator.xcframework/ios-arm64_x86_64-simulator/RandomColorGenerator.framework/Headers/RandomColorGenerator.h
@@ -0,0 +1,18 @@
+//
+//  RandomColorGenerator.h
+//  RandomColorGenerator
+//
+//  Created by Apple on 28/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RandomColorGenerator.
+FOUNDATION_EXPORT double RandomColorGeneratorVersionNumber;
+
+//! Project version string for RandomColorGenerator.
+FOUNDATION_EXPORT const unsigned char RandomColorGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomColorGenerator/PublicHeader.h>
+
+
